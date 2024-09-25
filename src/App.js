62e67f9a27393d903ed5526a1afecfb25a3b33a4
asaddev22 <asaddev22@gmail.com>
2024-09-25 21:59:55 +0100

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Home } from './Home';
+import {Navbar} from './Components/Navbar';
+import { Mars } from './Components/Mars';
+import { APOD } from './Components/APOD';
+import { NasaImg } from './Components/NasaImg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/mars" element={<Mars />} />
+        <Route path="/apod" element={<APOD />} />
+        <Route path="/nasa-img" element={<NasaImg />} />
+      </Routes>
+    </Router>
   );
 }
 
